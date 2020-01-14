b87ec3f9afe01169c70e82bd632e94bc4ac60aa1
shumbaashley <ashleytshumba@gmail.com>
2020-01-15 01:28:02 +0200

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn restaurant_system.wsgi --log-file -
--- restaurant_system/settings.py
+++ restaurant_system/settings.py
@@ -120,6 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASE['default'].update(db_from_env)
