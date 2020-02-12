94506dcf5d3aadcea263a10a082449fbbe432ba4
shumbaashley <ashleytshumba@gmail.com>
2020-02-12 11:23:05 +0200

    uppdated

--- restaurant_system/urls.py
+++ restaurant_system/urls.py
@@ -24,9 +24,23 @@ urlpatterns = [
 
     re_path(r'^api/social/', include('rest_framework_social_oauth2.urls')),
 
+    re_path(r'^api/restaurant/order/notification/(?P<last_request_time>.+)/$', apis.restaurant_order_notification),
+
+
     # API for Customers
     path('api/customer/restaurants/', apis.customer_get_restaurants),
     re_path(r'^api/customer/meals/(?P<restaurant_id>\d+)/$', apis.customer_get_meals),
     path('api/customer/order/add/', apis.customer_add_order),
     path('api/customer/order/latest/', apis.customer_get_latest_order),
+    path('api/customer/driver/location/', apis.customer_driver_location),
+
+    # API for Drivers
+    path('api/driver/order/ready/', apis.driver_get_ready_orders),
+    path('api/driver/order/pick/', apis.driver_pick_order),
+    path('api/driver/order/latest/', apis.driver_get_latest_order),
+    path('api/driver/order/complete/', apis.driver_get_complete_order),
+    path('api/driver/revenue/', apis.driver_get_revenue),
+    path('api/driver/location/update/', apis.driver_update_location),
+    
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- restaurants/serializers.py
+++ restaurants/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from restaurants.models import Restaurant, Meal
+from restaurants.models import Restaurant, Meal, Customer, Driver, Order, OrderDetails
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
@@ -26,6 +26,48 @@ class MealSerializer(serializers.ModelSerializer):
         model = Meal
         fields = ("id", "name", "short_description", "image", "price") 
 
+class OrderCustomerSerializer(serializers.ModelSerializer):
+    name= serializers.ReadOnlyField(source="user.get_full_name")
 
+    class Meta:
+        model = Customer
+        fields = ("id", "name", "avatar", "phone", "address")
+ 
+class OrderDriverSerializer(serializers.ModelSerializer):
+    name= serializers.ReadOnlyField(source="user.get_full_name")
 
+    class Meta:
+        model = Driver
+        fields = ("id", "name", "avatar", "phone", "address")
+ 
+
+class OrderRestaurantSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        fields = ("id", "name", "phone", "address")
         
+class OrderMealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meal
+        fields = ("id", "name", "price")
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    meal = OrderMealSerializer
+
+    class Meta:
+        model = OrderDetails
+        fields = ("id", "meal", "quantity", "sub_total")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    customer = OrderCustomerSerializer()
+    driver = OrderDriverSerializer()
+    restaurant = OrderRestaurantSerializer()
+    order_details = OrderDetailSerializer(many = True)
+    status = serializers.ReadOnlyField(source = "get_status_display")
+
+    class Meta:
+        model = Order
+        fields = ("id", "customer", "restaurant", "driver", "order_details", "total", "status", "address" )
+
+         
