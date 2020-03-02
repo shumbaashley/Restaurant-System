e5cafc7cb85313f68e7474a6d9c32d7871e840a5
shumbaashley <ashleytshumba@gmail.com>
2020-03-02 09:46:42 +0200

    sign in message update

--- restaurant_system/settings.py
+++ restaurant_system/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'restaurant_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db.sqlite3',
     }
 }
 
